1869325548295d7c30f47aa30ef6bad0f4816493
Nikolaj <Nikodyring@gmail.com>
2020-10-23 00:12:04 +0200

    feat(tailwind): add tailwind + remove Helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<template lang="pug">
+Navigation
+.container.mx-auto
+  router-view
 </template>
+
+<script lang="ts">
+import Navigation from '@/components/Navigation.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Navigation
+  }
+})
+
+</script>
 
 <style lang="stylus">
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
+  font-family "Montserrat", sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
 </style>
