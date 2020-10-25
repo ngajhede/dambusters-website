8c5d0753d00e9faed26b9688571f3a987be90aab
Nikolaj <Nikodyring@gmail.com>
2020-10-25 02:51:37 +0200

    add 404error component

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  NotFoundPage
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import NotFoundPage from '@/components/NotFoundPage.vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    NotFoundPage
+  }
+})
+</script>
