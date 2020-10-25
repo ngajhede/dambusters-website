c6b4b46fa095010e446db74a65b16c91001d4afb
Nikolaj <Nikodyring@gmail.com>
2020-10-25 02:03:42 +0200

    add card and hero to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,21 @@
 <template lang="pug">
-span Placeholder
+HeroCulture
+.container.mx-auto
+  .blog-posts
+    Card.inline-block.mx-3
+    Card.inline-block.mx-3
+    Card.inline-block.mx-3
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import HeroCulture from '@/components/HeroCulture.vue'
+import Card from '@/components/Card.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
+    Card, HeroCulture
   }
 })
 </script>
