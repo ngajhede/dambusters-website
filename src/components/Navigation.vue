353c8e13843f3ea36de95ec21e78543cb2a3a31c
Nikolaj <Nikodyring@gmail.com>
2020-10-25 03:07:18 +0100

    change text to white

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,17 +1,14 @@
 <template>
-<div class="container mx-auto">
-  <nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 sm:items-baseline w-full">
-    <div class="mb-2 sm:mb-0">
-      <a href="/" class="text-2xl no-underline text-black">The Dambusters</a>
-    </div>
-    <div class="content-end">
-      <a href="/#home" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">Home</a>
-      <a href="/#teams" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">Teams</a>
-      <a href="/#apply" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">Apply</a>
-      <a href="/#about" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">About</a>
-    </div>
-  </nav>
-</div>
+<nav class="font-sans flex flex-col sm:flex-row sm:text-left sm:justify-between py-4 px-6 sm:items-baseline w-full">
+  <div class="mb-2 sm:mb-0">
+    <a href="/" class="text-2xl no-underline text-white">The Dambusters</a>
+  </div>
+  <div class="content-end">
+    <a href="#home" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">Home</a>
+    <a href="#teams" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">Teams</a>
+    <a href="#about-us" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">About</a>
+  </div>
+</nav>
 </template>
 
 <script lang="ts">
@@ -22,3 +19,6 @@ export default defineComponent({
 })
 
 </script>
+
+<style lang="stylus">
+</style>
