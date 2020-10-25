0792e3ab04645c21af896dc48f005136e51e41f3
Nikolaj <Nikodyring@gmail.com>
2020-10-25 03:06:41 +0100

    add faq

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,28 @@
 <template lang="pug">
-HeroCulture
+HeroCulture(name="home")
 .container.mx-auto
-  .blog-posts
-    Card.inline-block.mx-3
-    Card.inline-block.mx-3
-    Card.inline-block.mx-3
+  .teams(name='teams')
+    TeamPicker
+  .about-us(name='about-us')
+    FAQList
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HeroCulture from '@/components/HeroCulture.vue'
 import Card from '@/components/Card.vue'
+import TeamPicker from '@/components/TeamPicker.vue'
+import FAQList from '@/components/FAQList.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-    Card, HeroCulture
+    Card, HeroCulture, TeamPicker, FAQList
   }
 })
 </script>
+
+<style lang="stylus">
+body
+  background-color #0a0a0a
+</style>
