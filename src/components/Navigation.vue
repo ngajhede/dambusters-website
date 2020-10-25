d69e4b357e0c87dc5fc136a956592108de04ef25
Nikolaj <Nikodyring@gmail.com>
2020-10-25 21:30:13 +0100

    change nav to be centered

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,14 +1,17 @@
 <template>
-<nav class="font-sans flex flex-col sm:flex-row sm:text-left sm:justify-between py-4 px-6 sm:items-baseline w-full">
-  <div class="mb-2 sm:mb-0">
-    <a href="/" class="text-2xl no-underline text-white">The Dambusters</a>
-  </div>
-  <div class="content-end">
-    <a href="#home" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">Home</a>
-    <a href="#teams" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">Teams</a>
-    <a href="#about-us" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">About</a>
-  </div>
-</nav>
+<div class="container mx-auto">
+  <nav class="font-sans flex flex-col sm:flex-row sm:text-left sm:justify-between py-4 px-6 sm:items-baseline w-full">
+    <div class="mb-2 sm:mb-0">
+      <a href="/" class="text-2xl no-underline text-white">The Dambusters</a>
+      <span class="text-white"> | # # # # </span>
+    </div>
+    <div class="content-end">
+      <a href="#home" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">Home</a>
+      <a href="#teams" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">Teams</a>
+      <a href="#about-us" class="text-lg no-underline text-gray-700 hover:text-red-600 ml-2">About</a>
+    </div>
+  </nav>
+</div>
 </template>
 
 <script lang="ts">
