5ec156465969b6c8d2bedabe6e56da059a3ee5d4
Nikolaj <Nikodyring@gmail.com>
2020-10-28 02:32:34 +0100

    add teams component

---
+++ src/views/Teams.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.teams.flex.min-h-screen.grid.grid-cols-3.container.mx-auto.mt-5.mb-5.shadow-2xl.rounded
+  TeamsInfo
+  TeamsList
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TeamsInfo from '@/components/Teams/TeamsInfo.vue'
+import TeamsList from '@/components/Teams/TeamsList.vue'
+
+export default defineComponent({
+  name: 'Teams',
+  components: {
+    TeamsInfo, TeamsList
+  }
+})
+</script>
+
+<style lang="stylus">
+.teams
+  background-color #141617
+</style>
