b5c991b34289d083a17b6d735d7996d632ecdd78
Nicolaj Gajhede <ngajhede@gmail.com>
2020-10-28 21:14:58 +0100

    add transition to burger menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -79,96 +79,98 @@
               ></path>
             </svg>
           </button>
-          <div v-if="isMenuOpen" class="absolute z-10 top-0 left-0 w-full">
-            <div class="p-5 bg-white border rounded shadow-sm">
-              <div class="flex items-center justify-between mb-4">
-                <div>
-                  <a
-                    href="/"
-                    aria-label="Company"
-                    title="Company"
-                    class="inline-flex items-center"
-                  >
-                    <svg
-                      class="w-8 text-deep-purple-accent-400"
-                      viewBox="0 0 24 24"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-miterlimit="10"
-                      stroke="currentColor"
-                      fill="none"
-                    >
-                      <rect x="3" y="1" width="7" height="12"></rect>
-                      <rect x="3" y="17" width="7" height="6"></rect>
-                      <rect x="14" y="1" width="7" height="6"></rect>
-                      <rect x="14" y="11" width="7" height="12"></rect>
-                    </svg>
-                    <span
-                      class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase"
-                      >The Dambusters</span
-                    >
-                  </a>
-                </div>
-                <div>
-                  <button
-                    aria-label="Close Menu"
-                    title="Close Menu"
-                    class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                    @click="isMenuOpen = false"
-                  >
-                    <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-                      <path
-                        fill="currentColor"
-                        d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-              <nav>
-                <ul class="space-y-4">
-                  <li>
+          <transition name="fade-down">
+            <div v-if="isMenuOpen" class="absolute z-10 top-0 left-0 w-full">
+              <div class="p-5 bg-white border rounded shadow-sm">
+                <div class="flex items-center justify-between mb-4">
+                  <div>
                     <a
                       href="/"
-                      aria-label="Our Home"
-                      title="Our Home"
-                      class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                      >Home</a
+                      aria-label="Company"
+                      title="Company"
+                      class="inline-flex items-center"
                     >
-                  </li>
-                  <li>
-                    <a
-                      href="/teams"
-                      aria-label="Our Home"
-                      title="Our Home"
-                      class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                      >Teams</a
-                    >
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      aria-label="About us"
-                      title="About us"
-                      class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                      >About us</a
-                    >
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                      aria-label="Sign up"
-                      title="Sign up"
-                    >
-                      Sign up
+                      <svg
+                        class="w-8 text-deep-purple-accent-400"
+                        viewBox="0 0 24 24"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-miterlimit="10"
+                        stroke="currentColor"
+                        fill="none"
+                      >
+                        <rect x="3" y="1" width="7" height="12"></rect>
+                        <rect x="3" y="17" width="7" height="6"></rect>
+                        <rect x="14" y="1" width="7" height="6"></rect>
+                        <rect x="14" y="11" width="7" height="12"></rect>
+                      </svg>
+                      <span
+                        class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase"
+                        >The Dambusters</span
+                      >
                     </a>
-                  </li>
-                </ul>
-              </nav>
+                  </div>
+                  <div>
+                    <button
+                      aria-label="Close Menu"
+                      title="Close Menu"
+                      class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                      @click="isMenuOpen = false"
+                    >
+                      <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                        <path
+                          fill="currentColor"
+                          d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <nav>
+                  <ul class="space-y-4">
+                    <li>
+                      <a
+                        href="/"
+                        aria-label="Our Home"
+                        title="Our Home"
+                        class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        >Home</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        href="/teams"
+                        aria-label="Our Home"
+                        title="Our Home"
+                        class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        >Teams</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        href="/"
+                        aria-label="About us"
+                        title="About us"
+                        class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        >About us</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        href="/"
+                        class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                        aria-label="Sign up"
+                        title="Sign up"
+                      >
+                        Sign up
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             </div>
-          </div>
+          </transition>
         </div>
       </div>
     </div>
@@ -195,4 +197,14 @@ export default defineComponent({
 
 .font-color
   color #e8e6e3
+
+.fade-down-enter-active,
+.fade-down-leave-active
+  transition all 0.2s ease-out
+
+.fade-down-enter-from,
+.fade-down-leave-to
+  transform translateY(-20px)
+  opacity 0
+
 </style>
