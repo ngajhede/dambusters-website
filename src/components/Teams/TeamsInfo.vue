48e5e24d0f2447a41e38d8dbddb89f5542484ef5
Nikolaj <Nikodyring@gmail.com>
2020-10-28 02:32:18 +0100

    add teams router

---
+++ src/components/Teams/TeamsInfo.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.teams-info.col-span-2.border-r-2.border-white
+  img(:src="require('@/assets/images/teams-background.jpg')").w-full.rounded-tl-lg
+  .text-white.m-3
+    span Contains all the sick team info
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TeamsInfo'
+})
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/components/Teams/TeamsList.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.teams-list.col-span-1
+  .text-white.m-5
+    span Ironwall
+    br
+    span Wildwood
+    br
+    span Stonewrought
+    br
+    span Management
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TeamsList'
+})
+</script>
