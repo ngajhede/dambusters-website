2b0f837e4402a1d4e10190bbf1bf2769b61be10f
Nikolaj <Nikodyring@gmail.com>
2020-10-28 02:33:40 +0100

    import font and fix background color

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,12 @@ export default defineComponent({
 </script>
 
 <style lang="stylus">
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap')
+
 #app
   font-family 'Roboto', sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
+
+  background-color #0e0f0f
 </style>
