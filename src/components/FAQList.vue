bbe5ca46f42188a7c1e31b360b0a052c9b66020f
Nicolaj Gajhede <ngajhede@gmail.com>
2020-10-28 21:30:40 +0100

    animate arrow on open

--- src/components/FAQList.vue
+++ src/components/FAQList.vue
@@ -61,15 +61,12 @@
                         How does it work?
                     </h3>
                     <div class="rounded-full border w-7 h-7 flex items-center justify-center hover:bg-gray-200">
-                        <div x-show="selectedItem !== 3" class="rounded-full text-gray-500 w-7 h-7 flex items-center justify-center">
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </div>
-                        <div x-show="selectedItem === 3" class="rounded-full text-gray-500 w-7 h-7 flex items-center justify-center">
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        <div class="rounded-full text-gray-500 w-7 h-7 flex items-center justify-center">
+                            <svg :class="{ 'rotate-90': selectedItem === 3 }" class="w-6 h-6 transform duration-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
                         </div>
                     </div>
                 </header>
-                <div x-show="selectedItem === 3">
+                <div v-show="selectedItem === 3">
                     <div class="pl-8 pr-8 py-5 bg-gray-100 text-gray-700">
                         <p>
                             Our platform works with your content to provides insights and metrics on how you can grow your business and scale your infastructure.
@@ -85,6 +82,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
-  name: 'FAQList'
+  name: 'FAQList',
+  data () {
+    return {
+      selectedItem: null
+    }
+  }
 })
 </script>
