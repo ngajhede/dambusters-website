fabf7dadabbf904f6e8b12c356b5880ac9cfb6a2
Nikolaj <Nikodyring@gmail.com>
2020-10-31 20:29:40 +0100

    change teamsinfo component

--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,18 +1,15 @@
 <template lang="pug">
-.teams.flex.min-h-screen.grid.grid-cols-3.container.mx-auto.mt-5.mb-5.shadow-2xl.rounded
-  TeamsInfo
-  TeamsList
+TeamsInfo
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import TeamsInfo from '@/components/Teams/TeamsInfo.vue'
-import TeamsList from '@/components/Teams/TeamsList.vue'
 
 export default defineComponent({
   name: 'Teams',
   components: {
-    TeamsInfo, TeamsList
+    TeamsInfo
   }
 })
 </script>
